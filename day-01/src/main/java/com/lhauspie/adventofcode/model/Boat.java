6a013193a9cb2ed3ef7e3d9c1974ea5a8398967d
Logan HAUSPIE <logan.hauspie.pro@gmail.com>
2022-12-03 21:37:47 +0100

    findingTopThreeElvesInBoatWith5ElvesShouldReturnThreeElvesWithMoreCalories

--- day-01/src/main/java/com/lhauspie/adventofcode/model/Boat.java
+++ day-01/src/main/java/com/lhauspie/adventofcode/model/Boat.java
@@ -1,8 +1,14 @@
 package com.lhauspie.adventofcode.model;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Boat {
 
   private Elf elfWithMoreCalories;
+  private List<Elf> elves = new ArrayList<>();
 
   public Elf getElfWithMoreCalories() throws EmptyBoatException {
     if (boatIsEmpty()) {
@@ -15,9 +21,15 @@ public class Boat {
     if (boatIsEmpty() || elf.carriesMoreCaloriesThan(elfWithMoreCalories)) {
       elfWithMoreCalories = elf;
     }
+    elves.add(elf);
   }
 
   private boolean boatIsEmpty() {
     return elfWithMoreCalories == null;
+  }
+
+  public List<Elf> getTopThreeElves() {
+    elves.sort((o1, o2) -> Integer.compare(o2.getCarriedCalories().getValue(), o1.getCarriedCalories().getValue()));
+    return elves.stream().limit(3).collect(Collectors.toList());
   }
 }
--- day-01/src/test/java/com/lhauspie/adventofcode/BoatTest.java
+++ day-01/src/test/java/com/lhauspie/adventofcode/BoatTest.java
@@ -7,6 +7,8 @@ import com.lhauspie.adventofcode.model.Quantity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class BoatTest {
 
   @Test
@@ -38,5 +40,37 @@ public class BoatTest {
     boat.add(elfWith200Calories);
 
     Assertions.assertEquals(elfWith3000Calories.getCarriedCalories(), boat.getElfWithMoreCalories().getCarriedCalories());
+  }
+
+  @Test
+  public void findingTopThreeElvesInBoatWith5ElvesShouldReturnThreeElvesWithMoreCalories()
+      throws EmptyBoatException {
+    var boat = new Boat();
+
+    Elf elfWith3000Calories = new Elf();
+    elfWith3000Calories.count(Quantity.of(3000));
+    boat.add(elfWith3000Calories);
+
+    Elf elfWith10000Calories = new Elf();
+    elfWith10000Calories.count(Quantity.of(10000));
+    boat.add(elfWith10000Calories);
+
+    Elf elfWith100Calories = new Elf();
+    elfWith100Calories.count(Quantity.of(100));
+    boat.add(elfWith100Calories);
+
+    Elf elfWith500Calories = new Elf();
+    elfWith500Calories.count(Quantity.of(500));
+    boat.add(elfWith500Calories);
+
+    Elf elfWith200Calories = new Elf();
+    elfWith200Calories.count(Quantity.of(200));
+    boat.add(elfWith200Calories);
+
+    List<Elf> topThreeElvesWithMoreCalories = boat.getTopThreeElves();
+    Assertions.assertEquals(3, topThreeElvesWithMoreCalories.size());
+    Assertions.assertEquals(10000, topThreeElvesWithMoreCalories.get(0).getCarriedCalories().getValue());
+    Assertions.assertEquals(3000, topThreeElvesWithMoreCalories.get(1).getCarriedCalories().getValue());
+    Assertions.assertEquals(500, topThreeElvesWithMoreCalories.get(2).getCarriedCalories().getValue());
   }
 }
