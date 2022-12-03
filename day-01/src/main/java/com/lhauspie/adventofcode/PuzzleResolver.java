78b188bf99b1588698032aa09f97f29d20ab5aee
Logan HAUSPIE <logan.hauspie.pro@gmail.com>
2022-12-03 20:34:19 +0100

    puzzleResolverCanResolveOneElfInBoat

--- day-01/src/main/java/com/lhauspie/adventofcode/PuzzleResolver.java
+++ day-01/src/main/java/com/lhauspie/adventofcode/PuzzleResolver.java
@@ -1,12 +1,22 @@
 package com.lhauspie.adventofcode;
 
 import com.lhauspie.adventofcode.model.Boat;
+import com.lhauspie.adventofcode.model.Elf;
 import com.lhauspie.adventofcode.model.EmptyBoatException;
+import com.lhauspie.adventofcode.model.Quantity;
 
 import java.util.Scanner;
 
 public class PuzzleResolver {
-    public static void resolve(Scanner scanner) throws EmptyBoatException {
-        new Boat().getElfWithMoreCalories().getCarriedCalories();
+  public static Quantity resolve(Scanner scanner) throws EmptyBoatException {
+    Boat boat = new Boat();
+
+    if (scanner.hasNext()) {
+      var elf = new Elf();
+      elf.count(Quantity.of(scanner.nextInt()));
+      elf.count(Quantity.of(scanner.nextInt()));
+      boat.add(elf);
     }
+    return boat.getElfWithMoreCalories().getCarriedCalories();
+  }
 }
--- day-01/src/test/java/com/lhauspie/adventofcode/PuzzleResolverTest.java
+++ day-01/src/test/java/com/lhauspie/adventofcode/PuzzleResolverTest.java
@@ -1,6 +1,7 @@
 package com.lhauspie.adventofcode;
 
 import com.lhauspie.adventofcode.model.EmptyBoatException;
+import com.lhauspie.adventofcode.model.Quantity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +15,15 @@ public class PuzzleResolverTest {
                 """;
         var scanner = new Scanner(puzzleContent);
         Assertions.assertThrows(EmptyBoatException.class, () -> PuzzleResolver.resolve(scanner));
+    }
+
+    @Test
+    public void puzzleResolverCanResolveOneElfInBoat() throws EmptyBoatException {
+        var puzzleContent = """
+                10000
+                10000
+                """;
+        var scanner = new Scanner(puzzleContent);
+        Assertions.assertEquals(Quantity.of(20000), PuzzleResolver.resolve(scanner));
     }
 }
