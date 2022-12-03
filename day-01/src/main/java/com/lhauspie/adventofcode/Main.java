15089d7307b00fd825653a632c980eac73ffaa9e
Logan HAUSPIE <logan.hauspie.pro@gmail.com>
2022-12-03 21:57:10 +0100

    Main for resolving both puzzle of day 01

---
+++ day-01/src/main/java/com/lhauspie/adventofcode/Main.java
@@ -0,0 +1,20 @@
+package com.lhauspie.adventofcode;
+
+import com.lhauspie.adventofcode.model.EmptyBoatException;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) throws FileNotFoundException, EmptyBoatException {
+    InputStream firstPuzzleInput = ClassLoader.getSystemClassLoader().getResourceAsStream("input.txt");
+    System.out.println("First puzzle result : " + PuzzleResolver.resolveFirstPuzzle(new Scanner(firstPuzzleInput)).getValue());
+
+    InputStream secondPuzzleInput = ClassLoader.getSystemClassLoader().getResourceAsStream("input.txt");
+    System.out.println("Second puzzle result : " + PuzzleResolver.resolveSecondPuzzle(new Scanner(secondPuzzleInput)).getValue());
+  }
+
+  private static void resolveFirstPuzzle(Scanner scanner) {}
+}
