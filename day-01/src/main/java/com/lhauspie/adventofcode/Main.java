12452438e00de90ee395caca54ad358b41ea94da
Logan HAUSPIE <logan.hauspie.pro@gmail.com>
2022-12-04 10:34:59 +0100

    fix code format and useless Exception

--- day-01/src/main/java/com/lhauspie/adventofcode/Main.java
+++ day-01/src/main/java/com/lhauspie/adventofcode/Main.java
@@ -2,13 +2,12 @@ package com.lhauspie.adventofcode;
 
 import com.lhauspie.adventofcode.model.EmptyBoatException;
 
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException, EmptyBoatException {
+    public static void main(String[] args) throws EmptyBoatException {
         InputStream firstPuzzleInput = ClassLoader.getSystemClassLoader().getResourceAsStream("input.txt");
         System.out.println("First puzzle result : " + PuzzleResolver.resolveFirstPuzzle(new Scanner(firstPuzzleInput)).getValue());
 
--- day-02/src/main/java/com/lhauspie/adventofcode/Main.java
+++ day-02/src/main/java/com/lhauspie/adventofcode/Main.java
@@ -1,12 +1,11 @@
 package com.lhauspie.adventofcode;
 
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         InputStream firstPuzzleInput = ClassLoader.getSystemClassLoader().getResourceAsStream("input.txt");
         System.out.println("First puzzle result : " + PuzzleResolver.resolveFirstPuzzle(new Scanner(firstPuzzleInput)).getValue());
 
